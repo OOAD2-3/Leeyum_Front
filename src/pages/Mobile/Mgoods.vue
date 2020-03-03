3e494928c6aaf0c8fa1e22bf5ee89393eaf2f6b7
wuluchaoren <38044760+wuluchaoren@users.noreply.github.com>
2020-03-03 10:20:15 +0800

    [ALTER] 2020/03/03
    修改移动端样式

--- src/pages/Mobile/Mgoods.vue
+++ src/pages/Mobile/Mgoods.vue
@@ -45,17 +45,41 @@
         </div>
         <div class="mline"></div>
         <div class="mgoods_content">
-          <div class="mgoods_items" v-for="item in goodsItems">
-            <img class="mgoods_items_img" :src="item.pic_urls.length>0?item.pic_urls[0]:'http://leeyum-bucket.oss-cn-hangzhou.aliyuncs.com/default_front_file/404pic.png'" alt=""/>
-            <div class="mgoods_items_content">
-              <div class="mitems_line1">
-                <div class="mline1_title">{{item.title.substr(0,10)}}
-                  <span v-if="item.title.length>10">...</span>
+          <div class="mgoods_content_col1">
+            <div class="mgoods_items" v-for="item in goodsItems1">
+              <img class="mgoods_items_img" :src="item.pic_urls.length>0?item.pic_urls[0]:'http://leeyum-bucket.oss-cn-hangzhou.aliyuncs.com/default_front_file/404pic.png'" alt=""/>
+              <div class="mgoods_items_content">
+                <div class="mitems_line1">
+                  <div class="mline1_title">{{item.title}}
+                  </div>
+                  <div class="mline1_date">{{item.publish_time}}</div>
                 </div>
-                <div class="mline1_date">{{item.publish_time}}</div>
               </div>
             </div>
           </div>
+          <div class="mgoods_content_col2">
+            <div class="mgoods_items" v-for="item in goodsItems2">
+              <img class="mgoods_items_img" :src="item.pic_urls.length>0?item.pic_urls[0]:'http://leeyum-bucket.oss-cn-hangzhou.aliyuncs.com/default_front_file/404pic.png'" alt=""/>
+              <div class="mgoods_items_content">
+                <div class="mitems_line1">
+                  <div class="mline1_title">{{item.title}}
+                  </div>
+                  <div class="mline1_date">{{item.publish_time}}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+<!--          <div class="mgoods_items" v-for="item in goodsItems">-->
+<!--            <img class="mgoods_items_img" :src="item.pic_urls.length>0?item.pic_urls[0]:'http://leeyum-bucket.oss-cn-hangzhou.aliyuncs.com/default_front_file/404pic.png'" alt=""/>-->
+<!--            <div class="mgoods_items_content">-->
+<!--              <div class="mitems_line1">-->
+<!--                <div class="mline1_title">{{item.title.substr(0,10)}}-->
+<!--                  <span v-if="item.title.length>10">...</span>-->
+<!--                </div>-->
+<!--                <div class="mline1_date">{{item.publish_time}}</div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
         </div>
         <div class="mmore_goods" @click="mgetMoreGoods" id="mmoreGoods">加载更多...</div>
       </div>
@@ -74,7 +98,7 @@
       components: {BottomRouter, MMyHead, Tail},
       data() {
         return {
-          MsearchKeyWordOut:'',
+          MsearchKeyWordOut: '',
           activeIndex: '1',
           carousel_list: [
             '../../../static/picture/ads1.jpg',
@@ -82,11 +106,13 @@
             '../../../static/picture/ads3.jpg',
           ],
           goodsItems: [],
+          goodsItems1: [],
+          goodsItems2: [],
           maxPage: 1,
           more_goods: true,
           nowType: '',
-          nowKeyword:'',
-          nowTypeName:'热门'
+          nowKeyword: '',
+          nowTypeName: '热门'
         }
       },
       methods: {
@@ -112,64 +138,42 @@
           this.$router.push({name: name});
         },
         mgetMoreGoods: function () {
-          if (this.$route.params.choosenType&&this.$route.params.choosenTypeName) {
+          if (this.$route.params.choosenType && this.$route.params.choosenTypeName) {
             this.$data.nowType = this.$route.params.choosenType;
             this.$data.nowTypeName = this.$route.params.choosenTypeName;
           }
           if (this.$data.more_goods) {
             if (this.$data.nowType === '') {
-              if (this.$data.nowKeyword === '') {
-                this.$axios.get("/api/article/?page=" + this.$data.maxPage + "&page_size=10&is_main=1").then(res => {
-                  for (let i = 0; i < res.data.data.article_list.length; i++)
-                    this.$data.goodsItems.push(res.data.data.article_list[i]);
-                  if (!res.data.data.has_next_page) {
-                    document.getElementById("mmoreGoods").innerHTML = "已无更多";
-                    document.getElementById("mmoreGoods").style.cursor = "auto";
-                    this.$data.more_goods = false;
-                  } else this.$data.maxPage++;
-                }).catch(err => {
-                  console.log(err);
-                });
-              } else {
-                this.$axios.get("/api/article/?page=" + this.$data.maxPage + "&page_size=10&keyword=" + this.$data.nowKeyword).then(res => {
-                  for (let i = 0; i < res.data.data.article_list.length; i++)
-                    this.$data.goodsItems.push(res.data.data.article_list[i]);
-                  if (!res.data.data.has_next_page) {
-                    document.getElementById("mmoreGoods").innerHTML = "已无更多";
-                    document.getElementById("mmoreGoods").style.cursor = "auto";
-                    this.$data.more_goods = false;
-                  } else this.$data.maxPage++;
-                }).catch(err => {
-                  console.log(err);
-                });
-              }
-            } else {
-              if (this.$data.nowKeyword === '') {
-                this.$axios.get("/api/article/?page=" + this.$data.maxPage + "&page_size=10&is_main=1&category=" + this.$data.nowType).then(res => {
-                  for (let i = 0; i < res.data.data.article_list.length; i++)
-                    this.$data.goodsItems.push(res.data.data.article_list[i]);
-                  if (!res.data.data.has_next_page) {
-                    document.getElementById("mmoreGoods").innerHTML = "已无更多";
-                    document.getElementById("mmoreGoods").style.cursor = "auto";
-                    this.$data.more_goods = false;
-                  } else this.$data.maxPage++;
-                }).catch(err => {
-                  console.log(err);
-                });
-              } else {
-                this.$axios.get("/api/article/?page=" + this.$data.maxPage + "&page_size=10&is_main=1&category=" + this.$data.nowType + "&keyword=" + this.$data.nowKeyword).then(res => {
-                  for (let i = 0; i < res.data.data.article_list.length; i++)
-                    this.$data.goodsItems.push(res.data.data.article_list[i]);
-                  if (!res.data.data.has_next_page) {
-                    document.getElementById("mmoreGoods").innerHTML = "已无更多";
-                    document.getElementById("mmoreGoods").style.cursor = "auto";
-                    this.$data.more_goods = false;
-                  } else this.$data.maxPage++;
-                }).catch(err => {
-                  console.log(err);
-                });
-              }
+              this.$axios.get("/api/article/?page=" + this.$data.maxPage + "&page_size=10&is_main=1").then(res => {
+                for (let i = 0; i < res.data.data.article_list.length; i++) {
+                  this.$data.goodsItems.push(res.data.data.article_list[i]);
+                  if(i%2===0) this.$data.goodsItems1.push(res.data.data.article_list[i]);
+                  else this.$data.goodsItems2.push(res.data.data.article_list[i]);
+                }
+                if (!res.data.data.has_next_page) {
+                  document.getElementById("mmoreGoods").innerHTML = "已无更多";
+                  document.getElementById("mmoreGoods").style.cursor = "auto";
+                  this.$data.more_goods = false;
+                } else this.$data.maxPage++;
+              }).catch(err => {
+                console.log(err);
+              });
             }
+          } else {
+            this.$axios.get("/api/article/?page=" + this.$data.maxPage + "&page_size=10&is_main=1&category=" + this.$data.nowType).then(res => {
+              for (let i = 0; i < res.data.data.article_list.length; i++) {
+                this.$data.goodsItems.push(res.data.data.article_list[i]);
+                if(i%2!==0) this.$data.goodsItems1.push(res.data.data.article_list[i]);
+                else this.$data.goodsItems2.push(res.data.data.article_list[i]);
+              }
+              if (!res.data.data.has_next_page) {
+                document.getElementById("mmoreGoods").innerHTML = "已无更多";
+                document.getElementById("mmoreGoods").style.cursor = "auto";
+                this.$data.more_goods = false;
+              } else this.$data.maxPage++;
+            }).catch(err => {
+              console.log(err);
+            });
           }
         },
         getNewMsearchKeyWordOut: function (MsearchKeyWord) {
